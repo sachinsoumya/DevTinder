7b9b961588e884ef13eb901ce6a72a994377f21b
sachinsoumya <pandasoumyasachin@gmail.com>
2025-03-10 21:00:21 +0530

    created different routes

--- src/app.js
+++ src/app.js
@@ -6,21 +6,38 @@ const app = express();
 
 // console.log(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello from dashboard .....");
+// app.use("/", (req, res) => {
+//   res.send("Hello from dashboard .....");
+
+// });
+
+app.use("/home/123", (req, res) => {
+  res.send("Welcome to home 123💕💕😁");
 });
 
-app.get("/test", (req, res) => {
+app.use("/home", (req, res) => {
+  res.send("Hello from home😊☀️");
+});
+
+app.use("/test/12", (req, res) => {
+  res.send("This is test 2💓");
+});
+
+app.use("/test", (req, res) => {
   res.send("Hello world from express javascript..");
 });
 
-app.get("/hello", (req, res) => {
+app.use("/hello", (req, res) => {
   res.send("1");
 });
 
-app.get('/about' , (req , res)=>{
-    res.send("This is about route");
-})
+app.use("/about123", (req, res) => {
+  res.send("This is about route");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello from dashboard .....");
+});
 
 app.listen(7777, () => {
   console.log("server is sucessfully in port 7777....");
