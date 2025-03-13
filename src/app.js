542e9ae693b0fad9be5b6c0e7af40fde9a916f44
sachinsoumya <pandasoumyasachin@gmail.com>
2025-03-13 17:34:23 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,47 @@ const express = require("express");
 // console.log(typeof express);
 
 const app = express();
+//* /abc , /ac
+app.use("/ab?c", (req, res) => {
+  res.send({ name: "Abc", age: 12 });
+});
 
+app.use("/ab+c", (req, res) => {
+  res.send({ name: "xyz", address: "UK" });
+});
+
+app.use("/a(bc)+d", (req, res) => {
+  res.send({ name: "xyz", address: "UK" });
+});
+
+app.use("/ab*cd", (req, res) => {
+  res.send({ name: "mnc", address: "USA" });
+});
+
+app.get(/a/, (req, res) => {
+  res.send({ name: "pqr", address: "Paris" });
+});
+app.get(/.*fly$/, (req, res) => {
+  res.send({ name: "pqr", address: "Paris" });
+});
+app.get("/user", (req, res) => {
+  console.log(req.query);
+
+  res.send({ name: "pqr", address: "Paris" });
+});
+app.use("/user/:uid/:name/:password", (req, res) => {
+  console.log(req.query);
+
+  console.log(req.params);
+
+  res.send({ name: "efg", address: "Chichago" });
+});
+app.get(/.*fly$/, (req, res) => {
+  res.send({ name: "pqr", address: "Paris" });
+});
+app.get(/.*fly$/, (req, res) => {
+  res.send({ name: "pqr", address: "Paris" });
+});
 
 app.get("/user", (req, res) => {
   res.send({ firstName: "Sachin", lastName: "Panda" });
@@ -14,10 +54,9 @@ app.post("/user", (req, res) => {
   res.send("Saving data to database...");
 });
 
-app.put("/user",(req,res)=>{
-  res.send('users updated  successfully');
+app.put("/user", (req, res) => {
+  res.send("users updated  successfully");
 });
-
 
 app.delete("/user", (req, res) => {
   res.send("Deleted data from database successfully..");
