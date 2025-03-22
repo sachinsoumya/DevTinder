5ce73c2fc58a3391ecd6364cdec200e202572cf4
sachinsoumya <pandasoumyasachin@gmail.com>
2025-03-22 21:02:01 +0530

    get and post api

--- src/app.js
+++ src/app.js
@@ -14,7 +14,11 @@ console.log(require("./config/database"));
 
 require("./config/database");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
+  console.log(req);
+  console.log(req.body);
   // const userObj = {
   //   firstName : "John",
   //   lastName:"Smith",
@@ -22,12 +26,7 @@ app.post("/signup", async (req, res) => {
   //   password:"John1234"
   // }
   //* Creating a new instance of the user model
-  const user = new User({
-    firstName: "Surya",
-    lastName: "Kumar",
-    email: "Surya@example.com",
-    password: "Sun234",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
@@ -36,6 +35,46 @@ app.post("/signup", async (req, res) => {
     res.send(err.message);
   }
 });
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({ email: req.body.email });
+
+    // const user = await User.findOne({ email: req.body.email });
+
+    // if (!user) {
+    //   res.send("User not found");
+    // } else {
+    //   res.send(user);
+    // }
+
+    // const user = await User.findById({_id:req.body._id});
+
+    // console.log(user);
+    // res.send(user);
+
+    console.log(user);
+
+    if (user.length == 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//* Feed Api = getting
 
 connectDb()
   .then(() => {
